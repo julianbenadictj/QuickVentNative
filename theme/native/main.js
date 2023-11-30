bdb4f8784e437cd7a51d3de87987dfb42b41a47b
Caden Thieken <caden.a.thieken@gsk.com>
2023-11-30 23:29:36 +0000

    create draft header and navigation bottom panel

--- theme/native/main.js
+++ theme/native/main.js
@@ -1,1 +1,40 @@
-export {};
+import { Appearance, Platform } from "react-native";
+import "../../themesource/atlas_core/native/api";
+import {
+  brand,
+  darkMode,
+  backgroundDefaults,
+  background,
+  contrast,
+} from "./custom-variables";
+
+// Custom Classes
+export const qvScroll = {
+  container: {
+    backgroundColor: "#FFFFFF",
+  },
+};
+
+export const qvHomeLayoutContainer = {
+  container: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: background.primary,
+  },
+};
+
+export const qvHomeLayoutInternalContainer = {
+  container: {
+    backgroundColor: background.primary,
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+};
+
+export const qvHomeLayoutHeader = {
+  text: {
+    color: "white",
+  },
+};
